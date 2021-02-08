72eb1fac91ce027fdd04c55e71985a40a2f34c9a
ruot-nyak <nyak.g.ruot@gmail.com>
2021-02-08 15:21:24 -0800

    add_song, find_song

--- Playlist.py
+++ Playlist.py
@@ -8,21 +8,32 @@ class Playlist:
   # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
   def add_song(self, title):
-    pass
-
+    new_song = Song(title)
+    new_song.set_title(title)
+    new_song.set_next_song(self.__first_song)
+    self.__first_song = new_song
 
 
   # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
   def find_song(self, title):
-    pass
+    counter = 0
+    current_song = self.__first_song
+
+    while current_song.get_title() != title:
+        counter += 1
+        current_song = current_song.get_next_song()
+        if current_song == None:
+            return -1
+
+    if current_song.get_title() == title:
+        return counter
 
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
     pass
-
 
 
   # TODO: Create a method called length, which returns the number of songs in the playlist.
@@ -40,5 +51,3 @@ class Playlist:
 
   def print_songs(self):
     pass
-
-  
